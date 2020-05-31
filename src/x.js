1960305a17c31f1aec69f06802ffd0e23e0e6ae8
Wang-XS <Wang-XS@hotmail.com>
2020-05-31 13:32:53 +0800

    CSS

--- src/x.js
+++ src/x.js
@@ -1,1 +1,2 @@
+import  './x.css' //引入css
 export default 'xxx'  //导出
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: '[name].[contenthash].js',
-    }
-  };
+      filename: 'index.[contenthash].js',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '13jun',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],//style-loader 把读到的字符串文件放到html里面,css-loader读取css文件
+          },
+        ],
+      },
+};
